996f6f8d605e6eec47ec9719616c7fd5310c53d8
Milena Napierała <m.napierala24@gmail.com>
2023-02-18 19:35:16 +0100

    scope-enclosing

--- 4-scope/scope-enclosing.py
+++ 4-scope/scope-enclosing.py
@@ -2,7 +2,7 @@
 #ENCLOSING:
 #inner - funkcja lokalna, dostępna tylko wewnątrz outer
 
-#TODO: print inner & outer x:
+#TODO 1: print inner & outer x:
 def outer():
     x = 'outer x'
 
@@ -14,3 +14,44 @@ def outer():
     print(x)
 
 outer()
+
+#TODO 2: print outer x:
+# def outer():
+#     x = 'outer x'
+
+#     def inner():
+#         nonlocal x
+#         print(x)
+
+#     inner()
+#     print(x)
+
+# outer()
+
+#TODO 3: print inner & outer & global:
+# x = 'global x'
+# def outer():
+#     x = 'outer x'
+
+#     def inner():
+#         x = 'inner x'
+#         print(x)
+
+#     inner()
+#     print(x)
+
+# outer()
+# print(x)
+
+#TODO 4: print global:
+# x = 'global x'
+# def outer():
+
+#     def inner():
+#         print(x)
+
+#     inner()
+#     print(x)
+
+# outer()
+# print(x)
