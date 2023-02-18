4c80b24ea18c51771c4d76625b641ee1f30a3e78
Milena Napierała <m.napierala24@gmail.com>
2023-02-18 18:21:35 +0100

    snippets-scopes(builtin)

--- 1-zadanka/20-scope-builtin.py
+++ 1-zadanka/20-scope-builtin.py
@@ -1,6 +1,12 @@
 #LEGB - Local, Enclosing, Global, Built-in
 #BUILT-IN:
 
-#TODO 1:
+import builtins
+#print(dir(builtins))
+
+#TODO: print minimalna(najmniejsza) liczba:
+def min():
+    pass
+
 m = min([5, 1, 4, 2, 3])
 print(m)
